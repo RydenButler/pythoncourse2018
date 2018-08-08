d7a98c7b05c3008f1fe13b6b4db698d886a9a211
Ryden Butler <ryden.winfield@gmail.com>
2018-08-07 20:45:57 -0500

    Lab 2 Solution
    Solution to lab 2 exercises

---
+++ day02/lab02_Ryden.py
@@ -0,0 +1,45 @@
+## Fill in the following methods for the class 'Clock'
+
+class Clock(object):
+    def __init__(self, hour, minutes):
+        self.minutes = minutes
+        self.hour = hour
+
+    @classmethod
+    def at(cls, hour, minutes=0):
+        return cls(hour, minutes)
+
+    ## Print the time
+    def __str__(self):
+    	return "%d:%02d" % (self.hour, self.minutes)
+    
+    ## Add time
+    ## Don't return anything
+    def __add__(self,minutes):
+    	self.minutes += minutes
+    	while self.minutes >= 60:
+    		self.hour += 1
+    		self.minutes -= 60
+    	while self.hour >= 24:
+    		self.hour -= 24
+    
+    ## Subtract time
+    ## Don't return anything
+    def __sub__(self,minutes):
+    	self.minutes -= minutes
+    	while self.minutes < 0:
+    		self.hour -= 1
+    		self.minutes += 60
+    	while self.hour < 0:
+    		self.hour += 24
+    
+    ## Are two times equal?
+    def __eq__(self, other):
+    	if self.minutes == other.minutes and self.hour == other.hour:
+    		return True
+    	else:
+    		return False
+    
+    ## Are two times not equal?
+    def __ne__(self, other):
+    	return not self.__eq__(other)
